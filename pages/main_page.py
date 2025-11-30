fde95c06bd0cf1c4ae7875701a3f586ddc06808b
qaingeener-create <qaingeener@gmail.com>
2025-11-30 10:19:32 +0300

    pages out

---
+++ pages/main_page.py
@@ -0,0 +1,49 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+
+class MainPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+        
+    def open(self):
+        # код для открытия страницы
+        pass
+
+    def click_arrow(self, question_number):
+        # код для клика по стрелке вопроса
+        pass
+
+    def is_text_opened(self, question_number):
+        # код для проверки, открылся ли текст вопроса
+        pass
+
+    # Локаторы для элементов на главной странице
+    ORDER_BUTTON_TOP = (By.CSS_SELECTOR, ".top-order-button")
+    ORDER_BUTTON_BOTTOM = (By.CSS_SELECTOR, ".bottom-order-button")
+    SCOOTER_LOGO = (By.ID, "scooter-logo")
+    YANDEX_LOGO = (By.ID, "yandex-logo")
+
+    def open(self):
+        # Метод для открытия главной страницы
+        pass
+
+    def click_order_button_top(self):
+        self.driver.find_element(*self.ORDER_BUTTON_TOP).click()
+        return OrderPage(self.driver)
+
+    def click_order_button_bottom(self):
+        self.driver.find_element(*self.ORDER_BUTTON_BOTTOM).click()
+        return OrderPage(self.driver)
+
+    def click_scooter_logo_and_check(self):
+        self.driver.find_element(*self.SCOOTER_LOGO).click()
+        # Проверка перехода на главную страницу "Самоката"
+        pass
+
+    def click_yandex_logo_and_check(self):
+        self.driver.find_element(*self.YANDEX_LOGO).click()
+        # Проверка перехода на главную страницу Дзена
+        pass
---
+++ pages/order_page.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+
+class OrderPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    # Локаторы для элементов на странице заказа
+    NAME_FIELD = (By.ID, "name")
+    SURNAME_FIELD = (By.ID, "surname")
+    ADDRESS_FIELD = (By.ID, "address")
+    METRO_STATION_SELECT = (By.ID, "metro-station")
+    PHONE_FIELD = (By.ID, "phone")
+    SUCCESS_MESSAGE = (By.CLASS_NAME, "success-message")
+
+    def fill_form(self, name, surname, address, metro_station, phone):
+        # Методы для заполнения полей формы заказа
+        self.driver.find_element(*self.NAME_FIELD).send_keys(name)
+        self.driver.find_element(*self.SURNAME_FIELD).send_keys(surname)
+        self.driver.find_element(*self.ADDRESS_FIELD).send_keys(address)
+        # Выбор станции метро из выпадающего списка
+        pass
+        self.driver.find_element(*self.PHONE_FIELD).send_keys(phone)
+
+    def is_success_message_present(self):
+        # Проверка наличия сообщения об успешном создании заказа
+        return self.wait.until(lambda driver: driver.find_element(*self.SUCCESS_MESSAGE).is_displayed())
