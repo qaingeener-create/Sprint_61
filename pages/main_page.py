ee806d69326add941b7ae3c4b64d9d9053725a4f
qaingeener-create <qaingeener@gmail.com>
2025-12-06 23:26:44 +0300

    вопросы о важном

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,12 +2,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
 
 
 class MainPage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        
 
     def open(self):
         self.driver.get("https://qa-scooter.praktikum-services.ru/")
@@ -15,27 +17,34 @@ class MainPage:
 
     def scroll_into_view(self):
     #Прокручивает страницу так, чтобы нужный элемент стал видимым.
-        element = self.driver.find_element(By.CLASS_NAME, "accordion")
+        element = self.driver.find_element(By.XPATH, '//div[contains(@class, "Home_FAQ")]')
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
-
-    def click_arrow(self, heading_id):
-    # Кликаем по заголовку вопроса с указанным ID
-        arrow_selector = f"#{heading_id}"
-        arrow = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, arrow_selector)))
-        arrow.click()
-
-
-    
-   
+        time.sleep(3)  # Приостанавливает выполнение на 1 секунду
+        
     def open_all_questions(self):
         for i in range(8):  # Предполагаем, что у нас есть 8 вопросов
             heading_id = f"accordion__heading-{i}"
+            time.sleep(3)  # Приостанавливает выполнение на 1 секунду
             self.click_arrow(heading_id)
-        # Здесь можно добавить небольшую задержку, если необходимо
-        # time.sleep(1)
 
-    
-    
+
+    def click_arrow(self, heading_id):
+    # Кликаем по заголовку вопроса с указанным ID
+        arrow_selector = heading_id
+        arrow = WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, arrow_selector)))
+        arrow.click()
+
+    def is_text_opened(self, question_number):
+    # Ищем элемент с текстом вопроса по его номеру
+        question_text_selector = f"accordion__panel-{question_number}"
+        try:
+        # Ждём, пока элемент станет видимым
+            WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, question_text_selector)))
+            return True
+        except TimeoutException:
+            return False
+
+
 
     # Локаторы для элементов на главной странице
     ORDER_BUTTON_TOP = (By.CSS_SELECTOR, ".button.Button_Button__ra12g")
